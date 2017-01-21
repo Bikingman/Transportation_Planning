f6d07ff73662948f107d2ff89f9b545d6441d8a1
Bikingman <daniel.patterson87@gmail.com>
2017-01-21 21:37:10 +0000

    homework1

---
+++ homework1.r
@@ -0,0 +1,37 @@
+
+################### part 1
+
+#1a) Create vector 1:12, values = ^2, call this vector x
+x <- (c(1:12)^2)
+
+#1b) Print 1st:3rd
+print(a[c(1:3)])
+
+#1c) print 3rd,7th,1st
+print(a[c(3, 7, 1)])
+
+#1d) display boolean vector, indicated elemants = 100
+x==100
+
+#1e) single command to find position of value 100 
+which(x == "100")
+
+#1f) create y as revrese order of x  
+y <- rev(x)
+
+################## part 2
+
+"""
+2a)	Calculate total travel distance 
+(find the distance variable from codebook) for 
+each person (personID) and household (hhid) 
+(save them as distance_person and distance_household, respectively; 
+use a hhsurvey-trips file).  Provide a summary statistics of total 
+travel distance variable for person and household. 
+"""
+personID <- 
+  personID <- subset()
+
+hhid <-   
+  
+  
