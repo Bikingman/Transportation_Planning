06e0a7fda9d06c7eae307fd9dfd8cea222f63f20
Bikingman <daniel.patterson87@gmail.com>
2020-04-18 08:19:52 -0400

    Update create_network.bat

--- SQL/routing_in_dc/create_network.bat
+++ SQL/routing_in_dc/create_network.bat
@@ -1,8 +1,8 @@
 
 
 bzcat /Users/Bikingman/Documents/osm_data/district-of-columbia-latest.osm.bz2 > /Users/Bikingman/Documents/dc_osm_data.osm
-osm2pgrouting -f /Users/Bikingman/Documents/dc_osm_data.osm -h localhost -U postgres -d routing --schema dc_routing --prefix dc_ -p 5432 -W user --conf=/Users/Bikingman/Documents/GitHub/Transportation_Planning/SQL/routing_in_dc/default_config.xml
+rem osm2pgrouting -f /Users/Bikingman/Documents/dc_osm_data.osm -h localhost -U postgres -d routing --schema dc_routing --prefix dc_ -p 5432 -W user --conf=/Users/Bikingman/Documents/GitHub/Transportation_Planning/SQL/routing_in_dc/default_config.xml
 
 osm2pgrouting -f /Users/Bikingman/Documents/dc_osm_data.osm -h localhost -U postgres -d routing --schema dc_routing --prefix dc_b_ -p 5432 -W user --conf=/Users/Bikingman/Documents/GitHub/Transportation_Planning/SQL/routing_in_dc/bike_config.xml
 
-osm2pgrouting -f /Users/Bikingman/Documents/dc_osm_data.osm -h localhost -U postgres -d routing --schema dc_routing --prefix dc_v_ -p 5432 -W user --conf=/Users/Bikingman/Documents/GitHub/Transportation_Planning/SQL/routing_in_dc/veh_config.xml
+rem osm2pgrouting -f /Users/Bikingman/Documents/dc_osm_data.osm -h localhost -U postgres -d routing --schema dc_routing --prefix dc_v_ -p 5432 -W user --conf=/Users/Bikingman/Documents/GitHub/Transportation_Planning/SQL/routing_in_dc/veh_config.xml
