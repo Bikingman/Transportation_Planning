655f0d94f02fcef5750de9a3bea02d46c06a08e7
Bikingman <daniel.patterson87@gmail.com>
2020-06-25 09:30:54 -0400

    Update helpful_commands.sql

--- SQL/helpful_commands.sql
+++ SQL/helpful_commands.sql
@@ -1,8 +1,16 @@
 
 
 -- get list of column names 
-SELECT *
-  FROM information_schema.columns
- WHERE table_schema = 'your_schema'
-   AND table_name   = 'your_table'
-     ;
+-- source https://dataedo.com/kb/query/postgresql/list-columns-names-in-specific-table
+select ordinal_position as position,
+       column_name,
+       data_type,
+       case when character_maximum_length is not null
+            then character_maximum_length
+            else numeric_precision end as max_length,
+       is_nullable,
+       column_default as default_value
+from information_schema.columns
+where table_name = 'roads'  
+  and table_schema= 'automated'
+order by ordinal_position;
