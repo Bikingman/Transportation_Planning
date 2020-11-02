50b797202d84a5ebe827c2592aa3c303de5b0c4f
Bikingman <daniel.patterson87@gmail.com>
2020-11-02 08:57:55 -0500

    improve error handling

--- R/shiny/data_exploration/get_extension.R
+++
@@ -1,4 +0,0 @@
- getExtension <- function(input) {
-    ex <- strsplit(basename(input), split = "\\.")[[1]]
-    return(ex[-1])
-}
--- R/shiny/data_exploration/observe_csv.R
+++ R/shiny/data_exploration/observe_csv.R
@@ -1,10 +1,16 @@
+#TODO abstract this so it can handle any datatype
 observe_csv <- function(session, input, output){
+  #create helper function
+  getExtension <- function(input) {
+    ex <- strsplit(basename(input), split = "\\.")[[1]]
+    return(ex[-1])
+  }
   #if the data is empty 
   loaded_extensions <- list()
    for (e in 1:length(input$csv[, 1])) {
      loaded_extensions[[e]] <- getExtension(input$csv[[e, 'datapath']])
    }
-  print(isTRUE(length(which(loaded_extensions == 'csv')) == length(loaded_extensions)))
+  
   if (is.null(input$csv)) {
     print("You selected nothing")
     showModal(modalDialog(
@@ -16,7 +22,6 @@ observe_csv <- function(session, input, output){
   } 
   # else if the data select is not a csv 
   else if (isTRUE(length(which(loaded_extensions == 'csv')) == length(loaded_extensions)) != TRUE) {
-    #TODO relace with modal 
     print("Incorrect file format")
     showModal(modalDialog(
       title = "Incorrect file format.",
@@ -25,23 +30,42 @@ observe_csv <- function(session, input, output){
       footer = NULL
     ))
   }
-  
   # else load the csv 
-  else {
+  else if ( isTRUE(length(which(loaded_extensions == 'csv')) == length(loaded_extensions)) == TRUE ) {
     #create empty list
     loaded_names <- list()
     upload <- list()
     #load csv files 
-    for (nr in 1:length(input$csv[, 1])) {
-      loaded_names[[nr]] <- sub(".csv$", "", basename(input$csv$name[[nr]]))
-      upload[[nr]] <- read.csv(
-        file = input$csv[[nr, 'datapath']], 
-        header = T, 
-        sep = ',',
-      )
-      names(upload)[nr] <- loaded_names[[nr]]
-    }
-    
+    tryCatch({ # setup catch for error
+      for (nr in 1:length(input$csv[, 1])) {
+        loaded_names[[nr]] <- sub(".csv$", "", basename(input$csv$name[[nr]]))
+        upload[[nr]] <- read.csv(
+          file = input$csv[[nr, 'datapath']], 
+          header = T, 
+          sep = ',',
+        )
+        names(upload)[nr] <- loaded_names[[nr]]
+        }
+      }, 
+      error = function(e) {
+        showModal(modalDialog(
+          title = "Incorrect file format.",
+          paste0("Something about your CSV is not correct."),
+          easyClose = TRUE,
+          footer = NULL
+        ))
+      }
+    )
+  } else {
+    print("Something's not correct")
+    showModal(modalDialog(
+      title = "Incorrect file format.",
+      paste0("Check your file format. Note, this app requires no '.' exist in your CSV file name other than for the file type extension."),
+      easyClose = TRUE,
+      footer = NULL
+    ))
+  }
+  
     
     upload <<- upload
     loaded_names <<- loaded_names
@@ -51,4 +75,3 @@ observe_csv <- function(session, input, output){
                       selected = loaded_names[1]
     )
 } 
-}
